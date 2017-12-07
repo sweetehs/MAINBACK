9fbac62e878cff65e3416313550eecc7021ccb59
赵伟男 <zhaoweinan@corp.netease.com>
2017-12-07 20:29:03 +0800

    ~

--- src/server/dist/front/src/components/widgets/list/ajax.js
+++ src/server/dist/front/src/components/widgets/list/ajax.js
@@ -1,11 +1,17 @@
 export default function init() {
     return {
-        name: 'ajax',        
-        data: {            
-           
-        },                       
+        name: 'ajax',
+        data: {
+            ajaxData: {
+                url: "http://www.test.com",
+                method: "get",
+                params: [{
+                    key: "key"
+                }]
+            }
+        },
         view: () => {
-            return `<Ajaxview></Ajaxview>`
+            return `<Ajaxview :defaultData="defaultData"></Ajaxview>`
         }
     }
 }
--- src/server/dist/front/src/components/widgets/set/ajaxView.vue
+++ src/server/dist/front/src/components/widgets/set/ajaxView.vue
@@ -4,10 +4,28 @@
 <template>
     <div>
         <div class="right-form-item">
+            <span class="text">method：</span>
+            <div class="content">
+                <el-radio v-model="defaultData.ajaxData.method" label="get">get</el-radio>
+                <el-radio v-model="defaultData.ajaxData.method" label="post">post</el-radio>                                  
+            </div>
+        </div>
+        <div class="right-form-item">
             <span class="text">URL：</span>
             <div class="content">
-                <el-input class="large" placeholder="请输入url"></el-input>           
+                <el-input class="large" placeholder="请输入url" v-model="defaultData.ajaxData.url"></el-input>           
+            </div>
+        </div>
+        <div class="right-form-item">
+            <span class="text">params：</span>
+            <div class="content">
+                <el-input v-for="(item,index) in defaultData.ajaxData.params" :key="index" class="large fn-mb5" placeholder="请输入参数" v-model="item.key"></el-input>                          
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default{
+        props:["defaultData"]
+    }
+</script>
