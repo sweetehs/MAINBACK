b6e1e9aeef8bf1826ad3296076492e8e179c6116
赵伟男 <zhaoweinann@126.com>
2018-01-17 08:41:28 +0800

    /Users/zhaoweinan

--- src/server/dist/front/src/_autocomponents/TEST/TEST.vue
+++ src/server/dist/front/src/_autocomponents/TEST/TEST.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
         <layout :style="baseData['c0'].styles">
-            <layout :style="baseData['c12217585'].styles">
-                <el-input v-model='c52397920model' :style="baseData['c52397920'].styles" placeholder="请输入文字" type="input" size="medium"  ></el-input>
-                <el-button  @click="c24936243click" :style="baseData['c24936243'].styles" type="primary" size="medium">默认</el-button>
-                <Table :ajaxd='c50278639model' :table="baseData['c50278639'].table"></Table>
+            <layout :style="baseData['c21721755'].styles">
+                <layout :style="baseData['c76734255'].styles">
+                    <el-button :style="baseData['c60490392'].styles" type="primary" size="medium">默认</el-button>
+                    <Table :ajaxd='c56509546model' :table="baseData['c56509546'].table"></Table>
+                    <el-input v-model='c84293572model' :style="baseData['c84293572'].styles" placeholder="请输入文字" type="input" size="medium"  ></el-input>
+                </layout>
+            </layout>    <layout :style="baseData['c37444039'].styles">
+                <el-button  @click="c33802866click" :style="baseData['c33802866'].styles" type="primary" size="medium">默认</el-button>
+                <Table :ajaxd='c97018897model' :table="baseData['c97018897'].table"></Table>
+                <layout :style="baseData['c87442295'].styles">
+        </layout>
             </layout>
         </layout>
     </div>
@@ -16,31 +23,30 @@
         data(){
             return {
                 baseData: baseData,
-                c52397920model:'',
-                c50278639model:''
+                c56509546model:'',
+                c84293572model:'',
+                c97018897model:''
             }
         },
         created(){
 
         },
         methods:{
-            c61707066ajax(){
+            c10653790ajax(){
                 axios({
-                    method: 'post',
+                    method: 'get',
                     url: 'http://www.test.com',
                     data: {
                         key:'testValue',key:'testValue',
                     },
-                    success(ajaxData){
-                        this.c50278639model = ajaxData.data.list        
-                    },
+                    success(ajaxData){},
                     error(){
                 
                     }
                 })
             },
-            c24936243click(){
-                this.c61707066ajax()
+            c33802866click(){
+                this.c10653790ajax()
             },
         }
     }
--- src/server/dist/front/src/_autocomponents/TEST/data.js
+++ src/server/dist/front/src/_autocomponents/TEST/data.js
@@ -1,7 +1,12 @@
 exports.baseData = {
-    c52397920:{"attr":{"placeholder":"请输入文字","type":"input","size":"medium","readonly":false},"styles":{}},
-    c24936243:{"attr":{"type":"primary","size":"medium","text":"默认"},"styles":{},"event":[{"type":"click","describe":"请输入描述","status":"add","action":[{"id":"c61707066","option":{"name":"ajax","data":{"ajaxData":{"describe":"详情","url":"http://www.test.com","method":"post","params":[{"key":"key","test":"testValue"},{"key":"key","test":"testValue"}],"link":{"id":"c50278639","pid":"c12217585","option":{"name":"table","data":{"table":{"describe":"描述","cells":[{"prop":"name","label":"名字"},{"prop":"sex","label":"姓别"}]},"attr":{},"styles":{},"event":[]},"staticConfig":{},"btmp":"<Table {{table|table}}></Table>"},"children":[]}}}}}]}]},
-    c50278639:{"table":{"describe":"描述","cells":[{"prop":"name","label":"名字"},{"prop":"sex","label":"姓别"}]},"attr":{},"styles":{},"event":[]},
-    c12217585:{"styles":{"height":"100%","width":"100%","display":"flex","flex":1,"flex-direction":"column"}},
-    c0:{"styles":{"height":"100%","width":"100%","display":"flex","flex":"none","flex-direction":"column"}},
+    c60490392:{"attr":{"type":"primary","size":"medium","text":"默认"},"styles":{},"event":[]},
+    c56509546:{"table":{"describe":"描述","cells":[{"prop":"name","label":"名字"},{"prop":"sex","label":"姓别"}]},"attr":{},"styles":{},"event":[]},
+    c84293572:{"describe":"","attr":{"placeholder":"请输入文字","type":"input","size":"medium","readonly":false},"styles":{}},
+    c76734255:{"styles":{"height":"100%","width":"100%","display":"flex","flex":1,"flex-direction":"column"}},
+    c21721755:{"styles":{"height":"100%","width":"200px","display":"flex","flex":"none","flex-direction":"column"}},
+    c33802866:{"attr":{"type":"primary","size":"medium","text":"默认"},"styles":{},"event":[{"type":"click","describe":"请输入描述","status":"add","action":[{"id":"c10653790","option":{"name":"ajax","data":{"ajaxData":{"describe":"ajax接口描述","url":"http://www.test.com","method":"get","params":[{"key":"key","test":"testValue"},{"key":"key","test":"testValue"}],"link":{}},"isCreated":false}}}]}]},
+    c97018897:{"table":{"describe":"描述","cells":[{"prop":"name","label":"名字"},{"prop":"sex","label":"姓别"}]},"attr":{},"styles":{},"event":[]},
+    c87442295:{"styles":{"height":"100%","width":"100%","display":"flex","flex":1,"flex-direction":"column"}},
+    c37444039:{"styles":{"height":"100%","width":"100%","display":"flex","flex":1,"flex-direction":"column"}},
+    c0:{"styles":{"height":"100%","width":"100%","display":"flex","flex":"none","flex-direction":"row"}},
 }
